b6ba6ff4b5940cfa067931bea0703f6fafbc6629
DennyJohnsonP <dennyjohnsondj4@gmail.com>
2021-06-08 18:31:53 +0530

    Shari1627 Mudule Integrated

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store','Portfolio',
+    'store','Portfolio','myapp',
     'django_inlinecss',
 ]
 
