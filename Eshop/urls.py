55ce0564f2fc7291e2c6123d5437ec3aefc711f6
DennyJohnsonP <dennyjohnsondj4@gmail.com>
2021-06-18 16:36:27 +0530

    a

--- Eshop/urls.py
+++ Eshop/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('',include('store.urls')),
     path("portfolio",include('Portfolio.urls')),
     path("contactus",include('myapp.urls')),
+    path("aboutus",include('website.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
